c55fd5115f31ea44fd78242acd82a4de887550ac
Malehek <yikmalehek23@gmail.com>
2025-04-17 15:48:40 +0300

    class pet created

--- week1.py
+++ week1.py
@@ -1,1 +1,13 @@
+# Creating a class Pet and initializing its attributes
 
+class Pet :
+    def __init__(self, name):
+        #setting the default values as 5 to start the pet in a balanced and neutral state
+        self.name = name
+        self.hunger = 5
+        self.energy = 5
+        self.happiness = 5
+        self.tricks = []
+
+
+# Methods        
