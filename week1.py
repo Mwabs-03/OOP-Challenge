56bdf957f1228616f10b0f0add1f07a12e20179f
nmwarish <nanimwarish@gmail.com>
2025-04-17 17:39:03 +0300

    i have updated methods, eat(), sleep(), play() and getStatus()

--- week1.py
+++ week1.py
@@ -11,3 +11,38 @@ class Pet :
 
 
 # Methods        
+# eat(): reduces hunger by 3 points (but not below 0), and increases happiness by 1.
+    def eat(self):
+        if self.hunger > 0:
+            self.hunger = max(self.hunger - 3, 0)
+            self.happiness += 1
+        else:
+            print(f"{self.name} is not hungry.")
+        return self.hunger, self.happiness
+    
+# sleep(): increases energy by 5 points (but not above 10).
+    def sleep(self):
+        self.energy = min(self.energy + 5, 10)
+        return self.energy
+    
+
+# play(): decreases energy by 2, increases happiness by 2, and increases hunger by 1.
+    def play(self):
+        # I have used 2 because, incase of any deductions, we  will not have negative energy
+        if self.energy >= 2:
+            self.energy -= 2
+            self.happiness += 2
+            self.hunger += 1
+        else:
+            print(f"{self.name} is too tired to play.")
+        return self.energy, self.happiness, self.hunger
+
+# get_status(): prints the current state of the pet.
+    def get_status(self):
+        print(f"Name: {self.name}")
+        print(f"Hunger: {self.hunger}")
+        print(f"Energy: {self.energy}")
+        print(f"Happiness: {self.happiness}")
+        # this line prints tricks , separated by a comma. 
+        # If there are no tricks, it prints 'None'.
+        print(f"Tricks: {', '.join(self.tricks) if self.tricks else 'None'}")
